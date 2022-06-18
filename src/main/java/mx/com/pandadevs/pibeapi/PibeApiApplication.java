6e411804f2079f95bc023239625e6f404cc41875
AlexisLoya <alexloy117@gmail.com>
2022-06-18 12:29:53 -0500

    Fist commit

---
+++ src/main/java/mx/com/pandadevs/pibeapi/PibeApiApplication.java
@@ -0,0 +1,13 @@
+package mx.com.pandadevs.pibeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/pandadevs/pibeapi/PibeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.pandadevs.pibeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PibeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
