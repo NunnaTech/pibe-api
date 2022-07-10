182be0071d36ad41ed7548ffb16fa3eb303f8b4f
Hector Saldaña Espinoza <20193tn070@utez.edu.mx>
2022-07-09 22:01:06 -0500

    Change implementation with data types I and T interface

--- src/main/java/mx/com/pandadevs/pibeapi/utils/interfaces/ControllerInterface.java
+++ src/main/java/mx/com/pandadevs/pibeapi/utils/interfaces/ControllerInterface.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 
-public interface ControllerInterface<T> {
+public interface ControllerInterface<I,T> {
     /** Controller Interface
      *  This interface helps to implement all repeated method each class
      * methods of a CRUD.
@@ -13,10 +13,10 @@ public interface ControllerInterface<T> {
      * the response through the Object ResponseEntity accompanied by an HTTP status.
      */
     ResponseEntity<List<T>> getAll();
-    ResponseEntity<T> getOne(Long id);
+    ResponseEntity<T> getOne(I id);
     ResponseEntity<T> save(T entity);
     ResponseEntity<T> update(T entity);
-    ResponseEntity<T> partialUpdate(Long id, Map<Object,Object> fields);
-    ResponseEntity<Boolean> delete(Long id);
+    ResponseEntity<T> partialUpdate(I id, Map<Object,Object> fields);
+    ResponseEntity<Boolean> delete(I id);
 
 }
