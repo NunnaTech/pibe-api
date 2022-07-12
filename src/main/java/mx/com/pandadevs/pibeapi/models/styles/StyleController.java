32e0f46c8c5a319e92938cc86a89b1bdaa461db6
AlexisLoya <alexloy117@gmail.com>
2022-07-12 15:21:11 -0500

    Add crud in StyleService

--- src/main/java/mx/com/pandadevs/pibeapi/models/styles/StyleController.java
+++ src/main/java/mx/com/pandadevs/pibeapi/models/styles/StyleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 @RestController
-@RequestMapping("style/")
+@RequestMapping("styles/")
 public class StyleController implements ControllerInterface<StyleDto, Integer> {
     @Autowired
     private StyleService styleService;
--- src/main/java/mx/com/pandadevs/pibeapi/models/styles/StyleRepository.java
+++ src/main/java/mx/com/pandadevs/pibeapi/models/styles/StyleRepository.java
@@ -3,6 +3,11 @@ package mx.com.pandadevs.pibeapi.models.styles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StyleRepository extends JpaRepository<Style, Integer> {
+    List<Style> findAllByActiveTrueOrderByCreatedAtDesc();
+    Optional<Style> findStyleByIdAndActiveTrue(Integer id);
 }
