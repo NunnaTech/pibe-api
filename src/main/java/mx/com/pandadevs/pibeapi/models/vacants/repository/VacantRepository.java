a76b6917f6f4fc02b2b35cecf00d7d8967cf6545
HectorSaldes <20193tn070@utez.edu.mx>
2022-08-25 11:00:54 -0500

    fix: Add only show vacants on active period (dates)

--- src/main/java/mx/com/pandadevs/pibeapi/models/vacants/repository/VacantRepository.java
+++ src/main/java/mx/com/pandadevs/pibeapi/models/vacants/repository/VacantRepository.java
@@ -4,11 +4,13 @@ import mx.com.pandadevs.pibeapi.models.vacants.entities.Vacant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VacantRepository extends JpaRepository<Vacant, Integer> {
     List<Vacant> findAllByActiveIsTrueAndIsPublicIsTrue();
+    List<Vacant> findAllByActiveIsTrueAndIsPublicIsTrueAndEndDateGreaterThanEqual(LocalDateTime time);
     Optional<Vacant> findByIdAndActiveIsTrue(Integer id);
 }
